36cc8485656d4967b3d0bc271c071b3000da9d76
Bernd Ruecker <bernd.ruecker@camunda.com>
2024-05-06 11:21:37 +0200

    .

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ async function run() {
       // The YML workflow will need to set myToken with the GitHub Secret Token
       // myToken: ${{ secrets.GITHUB_TOKEN }}
       // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-    const myToken = core.getInput('githubToken');
+    const myToken = core.getInput('github-token');
     console.log(myToken)
     const octokit = github.getOctokit(myToken);
 
