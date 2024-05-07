493d5fdd6e9f6c1523f7953474973df8eac333ed
Bernd Ruecker <bernd.ruecker@camunda.com>
2024-05-07 09:34:18 +0200

    .

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ const camunda = new camundaSdk.Camunda8({
     ZEEBE_ADDRESS: zeebeAddress,
     ZEEBE_CLIENT_ID: camundaClientId,
     ZEEBE_CLIENT_SECRET: camundaClientSecret,
-    ZEEBE_AUTHORIZATION_SERVER_URL: 'https://login.cloud.camunda.io/oauth/token'
+    ZEEBE_AUTHORIZATION_SERVER_URL: 'https://login.cloud.camunda.io/oauth/token',
+    CAMUNDA_OAUTH_URL: 'https://login.cloud.camunda.io/oauth/token',
   },
 })
 
