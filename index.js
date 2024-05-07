1208c531b85010de46f6bde4f85ef2236a2258ce
Bernd Ruecker <bernd.ruecker@camunda.com>
2024-05-07 09:23:55 +0200

    .

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const Camunda8 = require('@camunda8/sdk');
 
 
-    const camundaClientId = core.getInput('camunda-client-id');
-    const camundaClientSecret = core.getInput('camunda-client-secret');
-    const zeebeAddress = core.getInput('camunda-zeebe-address');
-    //const webmodelerClientId = core.getInput('webmodeler-client-id');
 
+const camundaClientId = core.getInput('camunda-client-id');
+const camundaClientSecret = core.getInput('camunda-client-secret');
+const zeebeAddress = core.getInput('camunda-zeebe-address');
+//const webmodelerClientId = core.getInput('webmodeler-client-id');
 
-import { Camunda8 } from '@camunda8/sdk'
 const camunda = new Camunda8({
   config: {
     ZEEBE_ADDRESS: zeebeAddress,
